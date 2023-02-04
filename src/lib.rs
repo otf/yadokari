505fd0c61139af3b1083e5ac4417c7ead033cd90
otf <otf@me.com>
2023-02-04 12:05:39 +0900

    post simple post.message

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,42 @@
-use axum::{routing::get, Router};
+use axum::{response::IntoResponse, routing::get, Router};
+use dotenv::dotenv;
+use reqwest::StatusCode;
+use serde::Serialize;
+use std::env;
 use sync_wrapper::SyncWrapper;
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+#[derive(Serialize)]
+struct PostMessageRequest {
+    text: String,
+    channel: String,
+}
+
+async fn post() -> impl IntoResponse {
+    dotenv().unwrap();
+    let token = env::var("SLACK_BOT_TOKEN").unwrap();
+    let client = reqwest::Client::new();
+    let post = PostMessageRequest {
+        text: "hi".to_owned(),
+        channel: "#bot-test".to_owned(),
+    };
+
+    let res = client
+        .post("https://slack.com/api/chat.postMessage")
+        .bearer_auth(&token)
+        .json(&post)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    (StatusCode::OK, res)
 }
 
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+    let router = Router::new().route("/post", get(post));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
