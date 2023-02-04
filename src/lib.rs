c8f9a8a4b6d80532b3613cc9e79aa2725f7f2de5
otf <otf@me.com>
2023-02-04 12:20:41 +0900

    add support to challenge by slack

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,11 @@
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
 use dotenv::dotenv;
 use reqwest::StatusCode;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::env;
 use sync_wrapper::SyncWrapper;
 
@@ -11,7 +15,38 @@ struct PostMessageRequest {
     channel: String,
 }
 
-async fn post() -> impl IntoResponse {
+#[derive(Deserialize)]
+struct ChallengeRequest {
+    token: String,
+    challenge: Option<String>,
+}
+
+#[derive(Serialize)]
+struct ChallengeResponse {
+    ok: bool,
+    challenge: Option<String>,
+}
+
+async fn challenge(Json(req): Json<ChallengeRequest>) -> impl IntoResponse {
+    let verification_token = env::var("VERIFICATION_TOKEN").unwrap();
+
+    if req.token != verification_token {
+        tracing::warn!("AuthenticationFailed, token {}", req.token);
+        let res = Json(ChallengeResponse {
+            ok: false,
+            challenge: None,
+        });
+        (StatusCode::BAD_REQUEST, res)
+    } else {
+        let res = Json(ChallengeResponse {
+            ok: true,
+            challenge: req.challenge,
+        });
+        (StatusCode::OK, res)
+    }
+}
+
+async fn post_message() -> impl IntoResponse {
     dotenv().unwrap();
     let token = env::var("SLACK_BOT_TOKEN").unwrap();
     let client = reqwest::Client::new();
@@ -36,7 +71,9 @@ async fn post() -> impl IntoResponse {
 
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
-    let router = Router::new().route("/post", get(post));
+    let router = Router::new()
+        .route("/challenge", post(challenge))
+        .route("/post", get(post_message));
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
